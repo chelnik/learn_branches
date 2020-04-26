b5e47185ad0c9746f7632b2dbe4278f0ad824138
Vadim Chelnik <sindbadseafarer@yandex.ru>
2020-04-26 13:34:23 +0300

    Изменил приветствие

---
+++ script.js
@@ -0,0 +1,9 @@
+/*var colors = ['red', 'green', 'blue', 'orange', 'yellow'];
+
+function getRandomIndex(arr){
+    let randomIndex = Math.random() * arr.length;
+    return arr(randomIndex)
+}
+varColor = getRandomIndex(colors);
+console.log(varColor);*/
+on
